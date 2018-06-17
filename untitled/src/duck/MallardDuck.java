ba03d23ad63e5789a5a5a4bde69a8ac4e93c8470
cumtwxl <745161263@qq.com>
2018-06-17 21:50:39 +0800

    Add files via upload

---
+++ untitled/src/duck/MallardDuck.java
@@ -0,0 +1,8 @@
+package duck;
+
+public class MallardDuck extends Duck {
+    @Override
+    public void display() {
+        System.out.println("绿头鸭");
+    }
+}
---
+++ untitled/src/duck/RedheadDuck.java
@@ -0,0 +1,8 @@
+package duck;
+
+public class RedheadDuck extends Duck {
+    @Override
+    public void display() {
+        System.out.println("红头鸭");
+    }
+}
